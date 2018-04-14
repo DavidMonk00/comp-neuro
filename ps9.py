d034a99e6da5d3b3bb99940dc090d3435a80e1cc
David Monk <d.g.monk0@gmail.com>
2018-04-14 11:10:56 +0100

    Removed idiosynchrosy in code

--- ps9.py
+++ ps9.py
@@ -1,36 +1,34 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import pylab as pl
 
 trials = 100
 time = 20
 time_reward = 20
 time_cue = 5
 time_endCue = time_reward
-n = time_endCue - time_cue + 1
+n = time_endCue - time_cue
 
 gamma = 1
-alpha = 0.6
+alpha = 0.8
 
 def main():
-    X = np.zeros((n, trials))
-    X[:,time_cue-1:time_endCue] = np.eye(n)
-    print X.shape
+    X = np.zeros((n, time))
+    X[:,time_cue:time_endCue] = np.eye(n)
     # print X[:,time_cue-1:time_endCue]
     V = np.zeros((time, trials))
-    w = np.zeros((n,1))
+    w = np.zeros(n)
     r = np.zeros((time,trials))
     r[time_reward-1,:] = 1
+    r[time_reward-1, 50] = 0
     delta = np.zeros((time, trials))
-
     for i in range(trials):
-        print w[:,0].shape, X[:,i].shape
-        print np.dot(w[:,0],X[:,i])
-        V[:,i] = np.dot(w[:,0], X[:,i])
-        print V.shape
+        V[:,i] = np.dot(w, X)
         delta[:,i] = r[:,i] + gamma * np.append(V[1:,i],0) - V[:,i]
-        print X.shape, delta[:,i].shape
         w = w + alpha*np.dot(X,delta[:,i])
-        # print V
+    pl.imshow(delta)
+    pl.show()
+    pl.imshow(V)
+    pl.show()
 
 if (__name__ == "__main__"):
     main()
